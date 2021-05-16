5d7de8cfa8577da06b86fa149201732548c84cd0
Heidi Fryzell <heidifryzell@MacBook-Air-2.local>
2021-05-16 10:59:48 -0400

    working code

--- js/script.js
+++ js/script.js
@@ -6,7 +6,28 @@ const img = document.querySelector(".img");
 const getImage = async function () {
     const res = await fetch("https://picsum.photos/v2/list?limit=100");
     const images = await res.json();
-    console.log(images);
+    selectRandomImage(images);
 };
 
-getImage();
+
+//pull a random whole number will be the index of the random image.
+const selectRandomImage = function(images) {
+    const randomIndex = Math.floor(Math.random() * images.length);
+    const randomImage = images[randomIndex];
+    displayImage(randomImage);
+};
+
+const displayImage = function (randomImage){
+    const author = randomImage.author;
+    const imageAddress = randomImage.download_url;
+    authorSpan.innerText = author;
+    imgDiv.classList.remove("hide");
+    img.setAttribute("src", imageAddress);
+    console.log(imageAddress);
+
+};
+
+button.addEventListener("click", function(){
+    getImage();
+});
+
